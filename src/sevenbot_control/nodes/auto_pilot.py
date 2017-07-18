e3f740d3053f93c8659388f0502391561019672e
Peng He <phe@hstartech.com>
2017-07-18 13:51:07 -0400

    add the action msg

--- src/sevenbot_control/nodes/auto_pilot.py
+++ src/sevenbot_control/nodes/auto_pilot.py
@@ -5,7 +5,9 @@ import roslib, rospy
 # Import the ROS messages
 from nav_msgs.msg import Odometry
 
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Twist, Point
+
+from math import radians, copysign, sqrt, pow, pi
 
 class Controller():
 
@@ -13,26 +15,64 @@ class Controller():
 
         # Setup initial variables
         self.dialength = rospy.get_param('~dialength', 0.0)                 # this should be measure from robot center
+        self.linear_speed = rospy.get_param('~linear_speed', 0.2)                 # this should be measure from robot center
+        self.angular_speed = rospy.get_param('~angular_speed', 1.0)                 # this should be measure from robot center
         # self.robot_base_length = rospy.get_param('~robot_base_length', 0.2) # hardcoded 20cm on y axis
         self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_cb, queue_size=2)    
 
         self.cmd_vel_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
+
+        self.angular_tolerance = radians(2.5)
+
+        # Initialize the tf listener
+        self.tf_listener = tf.TransformListener()
+
+        # Give tf some time to fill its buffer
+        rospy.sleep(2)
+
+        # Set the odom frame
+        self.odom_frame = '/odom'
+
+        self.old_position = Point()
+
+        # Find out if the robot uses /base_link or /base_footprint
+        try:
+            self.tf_listener.waitForTransform(self.odom_frame, '/base_footprint', rospy.Time(), rospy.Duration(1.0))
+            self.base_frame = '/base_footprint'
+        except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+            try:
+                self.tf_listener.waitForTransform(self.odom_frame, '/base_link', rospy.Time(), rospy.Duration(1.0))
+                self.base_frame = '/base_link'
+            except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+                rospy.loginfo("Cannot find transform between /odom and /base_link or /base_footprint")
+                rospy.signal_shutdown("tf Exception")  
 
     def odom_cb(self, data):
         pos_x = data.pose.pose.position.x
         quaternion = data.pose.pose.orientation
 
     def run_circle(self):
-        default_angular = 0.8 #rad/s
-        related_linear_x = self.dialength * default_angular
+        related_linear_x = self.dialength * self.angular_speed
 
         t = Twist()
         t.linear.x = related_linear_x
-        t.angular.z = default_angular
+        t.angular.z = self.angular_speed
 
         self.cmd_vel_pub.publish(t)
 
+    def go_straight(self, distance):
+        pass
+
+    def in_place_turn(self, radian):
+        pass        
+
     def run_square(self):
+        # simply setting up the square motion with odom data
+        # because if using Navigation Stack, robot needs a Lidar
+        for i in range(4):
+            self.go_straight(1.0)
+            self.in_place_turn(1.57)
+
         pass
 
 if __name__=='__main__':
